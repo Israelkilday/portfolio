25fcf4feb367592df9a57d272d6ee11ae659327d
Israel Kilday <israel.kilday@yahoo.com.br>
2023-10-07 21:31:50 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import Particles from 'react-tsparticles'
+import './App.css'
+
+import ParticlesBackground from './components/ParticlesBackground'
+
+function App() {
+
+  return (
+    <>
+      {/* <Particles /> */}
+      <ParticlesBackground />
+      <h1>ParticlesJS</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ParticlesBackground.jsx
@@ -0,0 +1,22 @@
+import React, { useCallback } from 'react';
+import Particles from "react-tsparticles"
+import { loadFull } from "tsparticles";
+import particlesConfid from './config/particlesConfig';
+
+const ParticlesBackground = () => {
+    const particlesInit = useCallback((engine) => {
+        loadFull(engine)
+    }, [])
+
+    return (
+        <div>
+            <Particles
+                // id="tsparticles"
+                options={particlesConfid}
+                init={particlesInit}
+            />
+        </div>
+    )
+}
+
+export default ParticlesBackground
