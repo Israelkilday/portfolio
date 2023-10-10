b4fa60159030a736ab5de27a252641c22799a95e
Israel Kilday <israel.kilday@yahoo.com.br>
2023-10-10 13:45:03 -0300

    finishin animations balls

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,12 +8,6 @@ import { FaMoon, FaSun, FaPalette } from "react-icons/fa6";
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
-    const [paletteOpen, setPaletteOpen] = useState(false);
-
-    const handlePaletteToggle = () => {
-        setPaletteOpen(!paletteOpen);
-    }
-
     return (
         <header className={styles.header}>
             <NavLink to={"/"} className={styles.logo}>
@@ -55,17 +49,31 @@ const Navbar = () => {
                 </label>
 
                 <label>
-                    <input onClick={handlePaletteToggle} 
-                           type="checkbox" 
-                           className={styles.input_palette_colors}
+                    <input 
+                        type="checkbox"
+                        className={styles.input_palette_colors}
                     />
 
                     <FaPalette className={styles.palette} />
 
-                    <div className={styles.toggle_palette_theme}></div>
-                    {paletteOpen && styles.toggle_palette_theme.palette_open}
+                    <div className={styles.toggle_palette_theme}>
+                        <button className={`${styles.ball} ${styles.ball_0}`}
+                            style={{ "--p": 1 }}
+                        ></button>
+                        <button className={`${styles.ball} ${styles.ball_1}`}
+                            style={{ "--p": 2 }}
+                        ></button>
+                        <button className={`${styles.ball} ${styles.ball_2}`}
+                            style={{ "--p": 3 }}
+                        ></button>
+                        <button className={`${styles.ball} ${styles.ball_3}`}
+                            style={{ "--p": 4 }}
+                        ></button>
+                        <button className={`${styles.ball} ${styles.ball_4}`}
+                            style={{ "--p": 5 }}
+                        ></button>
+                    </div>
                 </label>
-
             </div>
         </header>
     );
