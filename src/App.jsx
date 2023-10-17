d4107d471bb2e7e8284a9a223b349122dca9f2b0
Israel Kilday <israel.kilday@yahoo.com.br>
2023-10-17 14:47:05 -0300

    working in lightMode

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,23 @@
+// CSS
+import './App.css'
 // REACT ROUTER DOM
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 // COMPONENTS
 import Navbar from "./components/Navbar";
 // PAGES
 import Home from "./pages/Home/Home";
-// IMGAGENS
-// import Home_img from "./imgs/home_new_img2.png"
-// CSS
-import './App.css'
-// PARTICLES
-// import ParticlesBackground from './components/ParticlesBackground'
 
 function App() {
+  // <img className={darkMode ? 'light-mode-image' : 'dark-mode-image'} src={darkMode ? lightImageSrc : darkImageSrc} alt="Imagem" />
+
 
   return (
     <>
-      {/* <ParticlesBackground /> */}
       <BrowserRouter>
         <Navbar />
-        <div className="container">
+        <div className={`container ${lightMode ? "light_mode" : ""}`}>
           <Routes>
             <Route path="/" element={<Home />} />
-            {/*<Route />
-            <Route />
-            <Route /> */}
           </Routes>
         </div>
       </BrowserRouter>
