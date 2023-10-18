e75f40fbb9d40c0572e0dae08f7c38f3135569ec
Israel Kilday <israel.kilday@yahoo.com.br>
2023-10-17 23:51:38 -0300

    working in palette colors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 // HOOKS
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 // REACT ROUTER DOM
 import { NavLink } from "react-router-dom";
 // REACT ICONS
@@ -9,9 +9,22 @@ import styles from "./Navbar.module.css";
 
 const Navbar = () => {
     const [lightMode, setLightMode] = useState(false);
+    const [paletteOpen, setPaletteOpen] = useState(false);
+    const [selectedColor, setSelectedColor] = useState(null);
+    const [paletteInputInvisible, setPaletteInputInvisible] = useState(false);
 
-    const toggleLightMode = () => {
+
+    const handleToggleLightMode = () => {
         setLightMode(!lightMode);
+        setPaletteInputInvisible(!lightMode);
+    }
+
+    const handlePaletteToggle = () => {
+        setPaletteOpen(!paletteOpen)
+    }
+
+    const handleColorSelection = (color) => {
+        setSelectedColor(color);
     }
 
     useEffect(() => {
@@ -20,14 +33,29 @@ const Navbar = () => {
         } else {
             document.body.classList.remove('light_mode');
         }
-    }, [lightMode]);
-    
 
-    const [paletteOpen, setPaletteOpen] = useState(false);
+        if (selectedColor === "ball_0") {
+            document.documentElement.style.setProperty("--main_color", "#ffb703");
+        }
 
-    const handlePaletteToggle = () => {
-        setPaletteOpen(!paletteOpen)
-    }
+        if (selectedColor === "ball_1") {
+            document.documentElement.style.setProperty("--text_color", "#8ecae6");
+        }
+
+        if (selectedColor === "ball_2") {
+            document.documentElement.style.setProperty("--main_color", "#3a86ff");
+        }
+
+        if (selectedColor === "ball_3") {
+            document.documentElement.style.setProperty("--text_color", "#eb5e28");
+        }
+
+        if (selectedColor === "ball_4") {
+            document.documentElement.style.setProperty("--main_color", "#0ef");
+            document.documentElement.style.setProperty("--text_color", "#fff");
+        }
+
+    }, [lightMode, selectedColor]);
 
     return (
         <header className={styles.header}>
@@ -67,15 +95,14 @@ const Navbar = () => {
                     <input
                         type="checkbox"
                         className={styles.input_darc_light_mode}
-                        onClick={toggleLightMode}
+                        onClick={handleToggleLightMode}
                     />
 
                     <FaMoon className={styles.moon_icon} />
                     <FaSun className={styles.sun_icon} />
-                    {/* {lightMode ?  : } */}
                 </label>
 
-                <label>
+                <label style={{display: paletteInputInvisible ? "none" : ""}}>
                     <input
                         type="checkbox"
                         className={styles.input_palette_colors}
@@ -89,18 +116,27 @@ const Navbar = () => {
                     >
                         <button className={`${styles.ball} ${styles.ball_0}`}
                             style={{ "--p": 1 }}
+                            onClick={() => handleColorSelection("ball_0")}
                         ></button>
+
                         <button className={`${styles.ball} ${styles.ball_1}`}
                             style={{ "--p": 2 }}
+                            onClick={() => handleColorSelection("ball_1")}
                         ></button>
+
                         <button className={`${styles.ball} ${styles.ball_2}`}
                             style={{ "--p": 3 }}
+                            onClick={() => handleColorSelection("ball_2")}
                         ></button>
+
                         <button className={`${styles.ball} ${styles.ball_3}`}
                             style={{ "--p": 4 }}
+                            onClick={() => handleColorSelection("ball_3")}
                         ></button>
+
                         <button className={`${styles.ball} ${styles.ball_4}`}
                             style={{ "--p": 5 }}
+                            onClick={() => handleColorSelection("ball_4")}
                         ></button>
                     </div>
                 </label>
