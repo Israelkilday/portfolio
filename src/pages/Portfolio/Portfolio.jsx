b9ee045ec3acadbec1babfd7ba42929c43a4bd93
Israel Kilday <israel.kilday@yahoo.com.br>
2023-12-11 11:16:13 -0300

    catching from swiper

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -1,15 +1,11 @@
 // CSS
-import styles from "./Portfolio.module.css"
+import "./Portfolio.css"
 // HOOKS 
 import { useState, useRef } from "react";
 // COMPONENT
 import Transition from "../../components/Transition";
-// IMGAGENS
-import Portfolio_img from "../Home/imgs/home_test_img_2.0.png"
-import Portfolio_img1 from "../Home/imgs/teste00.png"
-import Portfolio_img2 from "../Home/imgs/teste01.png"
 // REACT ICONS
-import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+// import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 // SWIPER
 import { Swiper, SwiperSlide } from "swiper/react"
 import "swiper/css";
@@ -17,82 +13,63 @@ import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import { EffectCoverflow, Pagination, Navigation } from "swiper/modules"
+// IMGAGENS
+import Portfolio_img1 from "../Home/imgs/teste00.png"
 
 const Portfolio = () => {
-
   return (
     <Transition>
-      <section className={styles.portfolio}>
-        <div className={styles.container}>
-          <h1 className={styles.heading}>Principais projetos</h1>
-          <div className={styles.swiper}>
-            <Swiper
-              effect={"coverflow"}
-              grabCursor={true}
-              centeredSlides={true}
-              loop={true}
-              slidesPerView={"5"}
-              coverflowEffect={{
-                rotate: 0,
-                stretch: 0,
-                depth: 100,
-                modifier: 2.5,
-              }}
+      <section className="portfolio">
+        <div className="container">
+          <h1 className="heading">Principais projetos</h1>
+          <Swiper
+            effect={"coverflow"}
+            grabCursor={true}
+            centeredSlides={true}
+            loop={false}
+            slidesPerView={3}
+            // slidesPerGroup={3}
+            coverflowEffect={{
+              rotate: 0,
+              stretch: 0,
+              depth: 100,
+              modifier: 2.5,
+            }}
 
+            pagination={{ el: '.swiper-pagination', clickable: true }}
+            navigation={{
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
+              clickable: true,
+            }}
+            modules={[EffectCoverflow, Pagination, Navigation]}
+            className="swiper_container"
+          >
 
-              pagination={{ el: '.swiper_pagination', clickable: true }}
-              navigation={true}
-              // navigation={{
-              //   nextEl: '.swiper_button_next',
-              //   prevEl: '.swiper_button_prev',
-              //   clickable: true,
-              // }}
-              modules={[Navigation, Pagination, EffectCoverflow]}
-              className={styles.swiper_container}
-            >
+            <SwiperSlide>
+              <img src={Portfolio_img1} alt="project_0" />
+            </SwiperSlide>
 
-              <div>
-                <SwiperSlide className={styles.swiper_slide}>
-                  <img src={Portfolio_img1} alt="project_0" />
-                </SwiperSlide>
+            <SwiperSlide>
+              <img src={Portfolio_img1} alt="project_1" />
+            </SwiperSlide>
 
-                <SwiperSlide className={styles.swiper_slide}>
-                  <img src={Portfolio_img1} alt="project_1" />
-                </SwiperSlide>
+            <SwiperSlide>
+              <img src={Portfolio_img1} alt="project_2" />
+            </SwiperSlide>
 
-                <SwiperSlide className={styles.swiper_slide}>
-                  <img src={Portfolio_img1} alt="project_2" />
-                </SwiperSlide>
+            <div className="slider-controler">
+              <div className="swiper-button-prev slider-arrow">
+                <ion-icon name="arrow-back-outline"></ion-icon>
               </div>
 
-              {/* <SwiperSlide className={styles.swiper_slide}>
-              <img src={Portfolio_img2} alt="project_3" />
-            </SwiperSlide>
+              <div className="swiper-button-next slider-arrow">
+                <ion-icon name="arrow-forward-outline"></ion-icon>
+              </div>
 
-            <SwiperSlide className={styles.swiper_slide}>
-              <img src={Portfolio_img2} alt="project_4" />
-            </SwiperSlide>
-
-            <SwiperSlide className={styles.swiper_slide}>
-              <img src={Portfolio_img2} alt="project_5" />
-            </SwiperSlide> */}
-
-              {/* <div className={styles.slider_controler}>
-                <div className={styles.swiper_button_prev}>
-                  {/* <FaArrowLeft className={`${styles.swiper_button_prev} ${styles.slider_arrow}`} /> */}
-                  {/* <FaArrowLeft className={styles.slider_arrow} />
-                </div>
-
-                <div className={styles.swiper_button_next}>
-                  <FaArrowRight className={styles.slider_arrow} />
-                </div>  */}
-
-              {/* </div> */}
-                <div className={styles.swiper_pagination}></div>
-
-            </Swiper>
-
-          </div>
+              <div className="swiper-pagination"></div>
+            </div>
+          </Swiper>
         </div>
       </section>
     </Transition>
@@ -102,42 +79,6 @@ const Portfolio = () => {
 export default Portfolio;
 
 
-
-
-
-
-
-//   {
-//     name: "Project_1",
-//     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia magnam reprehenderit at aperiam expedita maxime ratione, et dolorum illum rerum quasi soluta corporis obcaecati fugiat laudantium consectetur perferendis provident quis?",
-//     image: Portfolio_img
-//   },
-//   {
-//     name: "Project_2",
-//     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia magnam reprehenderit at aperiam expedita maxime ratione, et dolorum illum rerum quasi soluta corporis obcaecati fugiat laudantium consectetur perferendis provident quis?",
-//     image: Portfolio_img1
-//   },
-//   {
-//     name: "Project_3",
-//     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia magnam reprehenderit at aperiam expedita maxime ratione, et dolorum illum rerum quasi soluta corporis obcaecati fugiat laudantium consectetur perferendis provident quis?",
-//     image: Portfolio_img2
-//   },
-//   {
-//     name: "Project_4",
-//     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia magnam reprehenderit at aperiam expedita maxime ratione, et dolorum illum rerum quasi soluta corporis obcaecati fugiat laudantium consectetur perferendis provident quis?",
-//     image: Portfolio_img
-//   },
-//   {
-//     name: "Project_5",
-//     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia magnam reprehenderit at aperiam expedita maxime ratione, et dolorum illum rerum quasi soluta corporis obcaecati fugiat laudantium consectetur perferendis provident quis?",
-//     image: Portfolio_img
-//   },
-//   {
-//     name: "Project_6",
-//     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia magnam reprehenderit at aperiam expedita maxime ratione, et dolorum illum rerum quasi soluta corporis obcaecati fugiat laudantium consectetur perferendis provident quis?",
-//     image: Portfolio_img
-//   }
-// ];
 
 //   const [currentIndex, setCurrentIndex] = useState(0);
 
