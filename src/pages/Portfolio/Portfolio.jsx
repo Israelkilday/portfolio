7775f04fe23dcd0b04b9f2865f301d05c781cf89
Israel Kilday <israel.kilday@yahoo.com.br>
2023-12-13 23:52:10 -0300

    working on resposive portfolio page

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -1,8 +1,10 @@
 // CSS
 import "./Portfolio.css"
-import  styles from "./Portfolio.module.css"
+import styles from "./Portfolio.module.css"
 // HOOKS 
 import { useState, useRef } from "react";
+// REACT ROUTER DOM
+import { NavLink } from "react-router-dom";
 // COMPONENT
 import Transition from "../../components/Transition";
 // REACT ICONS
@@ -18,59 +20,101 @@ import { EffectCoverflow, Pagination, Navigation } from "swiper/modules"
 import Portfolio_img1 from "../Home/imgs/teste00.png"
 
 const Portfolio = () => {
+
   return (
     <Transition>
       {/* <section className={styles.portfolio}> */}
-        <div className={styles.container}>
-          <h1 className="heading">Principais projetos</h1>
-          <Swiper
-            effect={"coverflow"}
-            grabCursor={true}
-            centeredSlides={true}
-            loop={false}
-            slidesPerView={3}
-            coverflowEffect={{
-              rotate: 0,
-              stretch: 0,
-              depth: 100,
-              modifier: 2.5,
-            }}
+      <div className={styles.container}>
+        <h1 className="heading"><span>//</span> Principais projetos</h1>
+        <Swiper
+          effect={"coverflow"}
+          grabCursor={true}
+          centeredSlides={true}
+          loop={false}
+          slidesPerView={3}
+          coverflowEffect={{
+            rotate: 0,
+            stretch: 0,
+            depth: 100,
+            modifier: 2.5,
+          }}
 
-            pagination={{ el: '.swiper-pagination', clickable: true }}
-            navigation={{
-              nextEl: '.swiper-button-next',
-              prevEl: '.swiper-button-prev',
-              clickable: true,
-            }}
-            modules={[EffectCoverflow, Pagination, Navigation]}
-            className="swiper_container"
-          >
-
-            <SwiperSlide>
+          pagination={{ el: '.swiper-pagination', clickable: true }}
+          navigation={{
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+            clickable: true,
+          }}
+          modules={[EffectCoverflow, Pagination, Navigation]}
+          className="swiper_container"
+        >
+          <SwiperSlide>
+            <div className={styles.teste}>
               <img src={Portfolio_img1} alt="project_0" />
-            </SwiperSlide>
 
-            <SwiperSlide>
-              <img src={Portfolio_img1} alt="project_1" />
-            </SwiperSlide>
-
-            <SwiperSlide>
-              <img src={Portfolio_img1} alt="project_2" />
-            </SwiperSlide>
-
-            <div className="slider-controler">
-              <div className="swiper-button-prev slider-arrow">
-                <ion-icon name="arrow-back-outline"></ion-icon>
+              <div className={styles.portfolio_content}>
+                <h4 className={styles.name}>Project_0</h4>
+                <p className={styles.description}>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque in, iusto nisi magnam laudantium facere quas adipisci sequi cumque.
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque in, iusto nisi magnam laudantium facere quas adipisci sequi cumque.
+                </p>
+                <div className={styles.links}>
+                  <NavLink className={styles.link}>Deploy</NavLink>
+                  <NavLink className={styles.link}>Saiba Mais</NavLink>
+                </div>
               </div>
-
-              <div className="swiper-button-next slider-arrow">
-                <ion-icon name="arrow-forward-outline"></ion-icon>
-              </div>
-
-              <div className="swiper-pagination"></div>
             </div>
-          </Swiper>
-        </div>
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <div className={styles.teste}>
+              <img src={Portfolio_img1} alt="project_1" />
+
+              <div className={styles.portfolio_content}>
+                <h4 className={styles.name}>Project_0</h4>
+                <p className={styles.description}>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque in, iusto nisi magnam laudantium facere quas adipisci sequi cumque.
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque in, iusto nisi magnam laudantium facere quas adipisci sequi cumque.
+                </p>
+                <div className={styles.links}>
+                  <NavLink className={styles.link}>Deploy</NavLink>
+                  <NavLink className={styles.link}>Saiba Mais</NavLink>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <div className={styles.teste}>
+              <img src={Portfolio_img1} alt="project_0" />
+
+              <div className={styles.portfolio_content}>
+                <h4 className={styles.name}>Project_2</h4>
+                <p className={styles.description}>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque in, iusto nisi magnam laudantium facere quas adipisci sequi cumque.
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque in, iusto nisi magnam laudantium facere quas adipisci sequi cumque.
+                </p>
+                <div className={styles.links}>
+                  <NavLink className={styles.link}>Deploy</NavLink>
+                  <NavLink className={styles.link}>Saiba Mais</NavLink>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+
+          <div className="slider-controler">
+            <div className="swiper-button-prev slider-arrow">
+              <ion-icon name="arrow-back-outline"></ion-icon>
+            </div>
+
+            <div className="swiper-button-next slider-arrow">
+              <ion-icon name="arrow-forward-outline"></ion-icon>
+            </div>
+
+            <div className="swiper-pagination"></div>
+          </div>
+        </Swiper>
+      </div>
       {/* </section> */}
     </Transition>
   );
