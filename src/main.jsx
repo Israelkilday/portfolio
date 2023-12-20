446157ae51901f345c993c65fc83bef9c9dd1ef8
Israel Kilday <israel.kilday@yahoo.com.br>
2023-12-20 16:35:37 -0300

    working swiper again

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,16 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
+import {register} from "swiper/element/bundle"
+
+register();
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "swiper/css/scrollbar";  
+import "swiper/css/effect-fade";
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -2,56 +2,139 @@
 import "./Portfolio.css"
 import styles from "./Portfolio.module.css"
 // HOOKS 
-import { useState, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 // REACT ROUTER DOM
 import { NavLink } from "react-router-dom";
 // COMPONENT
 import Transition from "../../components/Transition";
+
 // REACT ICONS
 // import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+
 // SWIPER
 import { Swiper, SwiperSlide } from "swiper/react"
-import "swiper/css";
-import "swiper/css/effect-coverflow";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-import { EffectCoverflow, Pagination, Navigation } from "swiper/modules"
+import { EffectFade, EffectFlip, EffectCoverflow } from "swiper/modules";
+// import "swiper/css/effect-coverflow";
+import { Pagination, Navigation, Scrollbar } from "swiper/modules"
 // IMGAGENS
 import Portfolio_img1 from "../Home/imgs/teste00.png"
 
 const Portfolio = () => {
+  // const [slidePerview, setSlidePerview] = useState(2);
+  const [slidePerview, setSlidePerview] = useState(1);
+
+  const data = [
+    {
+      id: 0,
+      image: Portfolio_img1,
+      name: "Project_0",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, laudantium illo. Omnis eos laborum saepe repellendus doloremque corporis laudantium voluptates temporibus eligendi, vitae rerum ea ab ad autem. Eius, maxime?"
+    },
+
+    {
+      id: 1,
+      image: Portfolio_img1,
+      name: "Project_1",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, laudantium illo. Omnis eos laborum saepe repellendus doloremque corporis laudantium voluptates temporibus eligendi, vitae rerum ea ab ad autem. Eius, maxime?"
+    },
+
+    {
+      id: 2,
+      image: Portfolio_img1,
+      name: "Project_2",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, laudantium illo. Omnis eos laborum saepe repellendus doloremque corporis laudantium voluptates temporibus eligendi, vitae rerum ea ab ad autem. Eius, maxime?"
+    },
+
+    {
+      id: 3,
+      image: Portfolio_img1,
+      name: "Project_3",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, laudantium illo. Omnis eos laborum saepe repellendus doloremque corporis laudantium voluptates temporibus eligendi, vitae rerum ea ab ad autem. Eius, maxime?"
+    }
+  ]
+
+  // useEffect(() => {
+  //   function handleResize() {
+  //     if (window.innerWidth < 720) {
+  //       setSlidePerview(1);
+  //     } else {
+  //       setSlidePerview(2);
+  //     }
+  //   }
+
+  //   handleResize();
+
+  //   window.addEventListener("resize", handleResize);
+
+  //   return () => {
+  //     window.removeEventListener("resize", handleResize)
+  //   }
+  // });
 
   return (
     <Transition>
-      {/* <section className={styles.portfolio}> */}
-      <div className={styles.container}>
+      {/* <div className={styles.container}> */}
+      <div className="container">
         <h1 className="heading"><span>//</span> Principais projetos</h1>
+
         <Swiper
-          effect={"coverflow"}
-          grabCursor={true}
-          centeredSlides={true}
-          loop={false}
-          slidesPerView={3}
-          coverflowEffect={{
-            rotate: 0,
-            stretch: 0,
-            depth: 100,
-            modifier: 2.5,
-          }}
-
-          pagination={{ el: '.swiper-pagination', clickable: true }}
-          navigation={{
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-            clickable: true,
-          }}
-          modules={[EffectCoverflow, Pagination, Navigation]}
-          className="swiper_container"
+          modules={[EffectCoverflow]}
+          effect="coverflow"
+          slidesPerView={slidePerview}
+          pagination={{ clickable: true }}
+          navigation
         >
-          <SwiperSlide>
-            <div className={styles.teste}>
-              <img src={Portfolio_img1} alt="project_0" />
+          {data.map((item) => (
+            <SwiperSlide key={item.id} >
+              <div className="portfolio_container">
+                <img src={item.image}
+                  alt={item.name}
+                  className="item_slide"
+                />
+                <div className="portfolio_content">
+                  <h2 className="name">{item.name}</h2>
+                  <p className="description">{item.description}</p>
 
+                  <div className="links">
+                  <NavLink className="link">Deploy</NavLink>
+                  <NavLink className="link">Saiba Mais</NavLink>
+                </div>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </Transition>
+  );
+};
+
+export default Portfolio;
+
+
+
+// effect={"coverflow"}
+// grabCursor={true}
+// centeredSlides={true}
+// loop={false}
+// slidesPerView={3}
+// coverflowEffect={{ 
+//   rotate: 0,
+//   stretch: 0,
+//   depth: 100,
+//   modifier: 2.5,
+// }}
+
+// pagination={{ el: '.swiper-pagination', clickable: true }}
+// navigation={{
+//   nextEl: '.swiper-button-next',
+//   prevEl: '.swiper-button-prev',
+//   clickable: true,
+// }}
+// modules={[EffectCoverflow, EffectFade, Pagination, Navigation]}
+// className="swiper_container"
+
+{/* <div className={styles.teste}>
               <div className={styles.portfolio_content}>
                 <h4 className={styles.name}>Project_0</h4>
                 <p className={styles.description}>
@@ -64,7 +147,6 @@ const Portfolio = () => {
                 </div>
               </div>
             </div>
-          </SwiperSlide>
 
           <SwiperSlide>
             <div className={styles.teste}>
@@ -112,15 +194,21 @@ const Portfolio = () => {
             </div>
 
             <div className="swiper-pagination"></div>
-          </div>
-        </Swiper>
-      </div>
-      {/* </section> */}
-    </Transition>
-  );
-};
+          </div> */}
 
-export default Portfolio;
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
