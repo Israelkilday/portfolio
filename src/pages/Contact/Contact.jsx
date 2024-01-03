e6cade835bf8fd2f851b9a98813355b6f35b438a
Israel Kilday <israel.kilday@yahoo.com.br>
2024-01-03 00:01:09 -0300

    finish contact page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -8,66 +8,40 @@ import Transition from "../../components/Transition";
 import emailjs from "@emailjs/browser"
 // SWEETALERT
 import Swal from 'sweetalert2';
-import withReactContent from 'sweetalert2-react-content';
 
 const Contact = () => {
-
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
   const [subject, setSubject] = useState("");
   const [message, setMessage] = useState("");
-  const [errorState, setErroState] = useState(false);
 
-  const addEventListeners = () => {
-    const inputFields = document.querySelectorAll(`.${styles.item}`);
-
-    inputFields.forEach((item) => {
-      item.addEventListener("keyup", () => {
-        if (item.value !== "") {
-          setErroState(false);
-        } else {
-          setErroState(true);
-        }
-      });
-    });
-  };
-
-  useEffect(() => {
-    addEventListeners();
-
-    return () => {
-      const inputFields = document.querySelectorAll(`.${styles.item}`);
-      inputFields.forEach((item) => {
-        item.removeEventListener("keyup", () => { });
-      });
-    };
-  }, []);
+  const [nameError, setNameError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [phoneError, setPhoneError] = useState(false);
+  const [subjectError, setSubjectError] = useState(false);
+  const [messageError, setMessageError] = useState(false);
 
   function sendEmail(e) {
     e.preventDefault();
 
-    const fieldsToCheck = [name, email, phone, subject, message];
+    // validateEmail();
 
-    if (fieldsToCheck.some(field => field === "")) {
-      setErroState(true);
-      return;
+    if (name === "" || email === "" || phone === "" || subject === "" || message === "") {
+      setNameError(name === "");
+      setEmailError(email === "");
+      setPhoneError(phone === "");
+      setSubjectError(subject === "");
+      setMessageError(message === "");
+
+      return
     }
 
-    // if (
-    //   name === "" ||
-    //   email === "" ||
-    //   phone === "" ||
-    //   subject === "" ||
-    //   message === ""
-    // ) {
-
-    //   setErroState(true);
-
-    //   return;
-    // }
-
-    setErroState(false);
+    setNameError(false);
+    setEmailError(false);
+    setPhoneError(false);
+    setSubjectError(false);
+    setMessageError(false);
 
     const templateParams = {
       from_name: name,
@@ -102,21 +76,18 @@ const Contact = () => {
     });
   };
 
-  // const checkInputs = () => {
-  //   item.addEventListener("keyup", () => {
-  //     if (item.value != "") {
-  //       setErroState(true);
-  //     } else {
-  //       setErroState(false);
-  //     }
-  //   });
-  // }
+  // const validateEmail = (email) => {
+  //   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
+  //   if (setEmail !== emailRegex ) {
+  //     setEmailError(true)
+  //   } 
+  //   return emailRegex.test(email);
+  // };
 
   return (
     <Transition>
       <section className={styles.contact}>
-
         <h2>
           <span>//</span>  Contate <span>Me!</span>
         </h2>
@@ -127,32 +98,38 @@ const Contact = () => {
               <input
                 type="text"
                 placeholder="Nome Completo"
-                className={`${styles.item} ${errorState ? styles.error : ""}`}
+                className={`${styles.item} ${nameError ? styles.error : ""}`}
                 id="name"
-                onChange={(e) => setName(e.target.value)}
+                onChange={(e) => {
+                  setName(e.target.value);
+                  setNameError(false);
+                }}
                 value={name}
               />
 
               <div
                 id="error_name"
-                className={`${styles.error_message} ${errorState ? styles.show_message : ""}`}
+                className={`${styles.error_message} ${nameError ? styles.show_message : ""}`}
               >
                 Nome não pode ficar em branco</div>
             </div>
 
             <div className={`${styles.input_field} ${styles.field}`}>
               <input
-                type="text"
+                type="email"
                 placeholder="Endereço do Email"
-                className={`${styles.item} ${errorState ? styles.error : ""}`}
+                className={`${styles.item} ${emailError ? styles.error : ""}`}
                 id="email"
-                onChange={(e) => setEmail(e.target.value)}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                  setEmailError(false);
+                }}
                 value={email}
               />
 
               <div
                 id="error_email"
-                className={`${styles.error_message} ${errorState ? styles.show_message : ""}`}
+                className={`${styles.error_message} ${emailError ? styles.show_message : ""}`}
               >
                 Email não pode ficar em branco
               </div>
@@ -165,15 +142,18 @@ const Contact = () => {
               <input
                 type="text"
                 placeholder="Número do Telefone"
-                className={`${styles.item} ${errorState ? styles.error : ""}`}
+                className={`${styles.item} ${phoneError ? styles.error : ""}`}
                 id="phone"
-                onChange={(e) => setPhone(e.target.value)}
+                onChange={(e) => {
+                  setPhone(e.target.value);
+                  setPhoneError(false);
+                }}
                 value={phone}
               />
 
               <div
                 id="error_phone"
-                className={`${styles.error_message} ${errorState ? styles.show_message : ""}`}
+                className={`${styles.error_message} ${phoneError ? styles.show_message : ""}`}
               >
                 Telefone não pode ficar em branco
               </div>
@@ -184,14 +164,17 @@ const Contact = () => {
                 type="text"
                 placeholder="Assunto"
                 id="subject"
-                className={`${styles.item} ${errorState ? styles.error : ""}`}
-                onChange={(e) => setSubject(e.target.value)}
+                className={`${styles.item} ${subjectError ? styles.error : ""}`}
+                onChange={(e) => {
+                  setSubject(e.target.value)
+                  setSubjectError(false);
+                }}
                 value={subject}
               />
 
               <div
                 id="error_subject"
-                className={`${styles.error_message} ${errorState ? styles.show_message : ""}`}
+                className={`${styles.error_message} ${subjectError ? styles.show_message : ""}`}
               >
                 Assunto não pode ficar em branco
               </div>
@@ -205,20 +188,25 @@ const Contact = () => {
               id="message"
               cols="30"
               rows="10"
-              className={`${styles.item} ${errorState ? styles.error : ""}`}
-              onChange={(e) => setMessage(e.target.value)}
+              className={`${styles.item} ${messageError ? styles.error : ""}`}
+              onChange={(e) => {
+                setMessage(e.target.value)
+                setMessageError(false);
+              }}
               value={message}
             >
             </textarea>
 
             <div
               id="error_message"
-              className={`${styles.error_message} ${errorState ? styles.show_message : ""}`}
+              className={`${styles.error_message} ${messageError ? styles.show_message : ""}`}
             >
               Mensagem não pode ficar em branco</div>
           </div>
 
-          <button type="submit">Enviar Mensagem</button>
+          <div className={styles.btn_box}>
+            <button className={styles.btn} type="submit">Enviar Mensagem</button>
+          </div>
         </form>
       </section>
     </Transition>
@@ -228,104 +216,5 @@ const Contact = () => {
 export default Contact;
 
 
-// function checkInputs() {
-//   const items = document.querySelectorAll(".item");
-
-//   for (const item of items) {
-//     if (item.value == "") {
-//       item.classList.add("error");
-//       item.parentElement.classList.add("error");
-//     }
-
-//     item.addEventListener("keyup", () => {
-//       if (item.value != "") {
-//         item.classList.remove("error");
-//         item.parentElement.classList.remove("error");
-
-//       } else {
-//         item.classList.add("error");
-//         item.parentElement.classList.add("error");
-//       }
-//     });
-//   };
-// } ;
 
 
-
-
-
-
-
-
-// useEffect(() => {
-//   const script = document.createElement('script');
-//   script.src = 'https://smtpjs.com/v3/smtp.js';
-//   script.async = true;
-//   document.body.appendChild(script);
-
-//   return () => {
-//     document.body.removeChild(script);
-//   };
-// }, []);
-
-// const form = document.querySelector("form");
-
-// function sendEmail(e) {
-//     e.preventDefault();
-
-//   if (name === "" || email === "" || phone === "") {
-//     alert("Prencha todos os campos")
-//   }
-
-//   Email.send({
-//     Host: "smtp.elasticemail.com",
-//     Username: "israel.kilday@yahoo.com.br",
-//     Password: "A7669DA9FADD95A336997B32007E62091E07A713173635C03487BE4AF8FBA7215C76D562545A2DCF10637A0931A79D99",
-//     To: 'israel.kilday@yahoo.com.br',
-//     From: "israel.kilday@yahoo.com.br",
-//     Subject: "This is the subject",
-//     Body: "And this is the body"
-//   }).then(
-//     message => alert(message)
-//     );
-//     console.log(message)
-// }
-
-// form.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   sendEmail();
-// });
-
-// A7669DA9FADD95A336997B32007E62091E07A713173635C03487BE4AF8FBA7215C76D562545A2DCF10637A0931A79D99
-
-
-{/* <div className={styles.input_box}>
-            
-              <input
-                type="text"
-                placeholder="Nome Completo"
-                className={styles.input_name}
-              />
-
-            <input type="email" placeholder="Email" className={styles.input_email} />
-          </div>
-
-          <div className={styles.input_box}>
-            <input type="number" placeholder="Telefone" className={styles.input_fone} />
-            <input type="email" placeholder="Assunto do Email" className={styles.input_subject} />
-          </div>
-
-          <textarea
-            className={styles.input_textarea}
-            name=""
-            id=""
-            cols="30"
-            rows="10"
-            placeholder="Sua Mensagem"
-          ></textarea>
-
-          <div className={styles.btn_box}>
-            <button type="submit" className={styles.btn}>
-              Enviar Mensagem
-            </button>
-          </div> */}
