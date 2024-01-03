02fb36bd0f204dd72f8e7a3bf2b12b2abbf92ecf
Israel Kilday <israel.kilday@yahoo.com.br>
2024-01-03 18:58:23 -0300

    finish contact page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -2,12 +2,18 @@
 import styles from "./Contact.module.css"
 // HOOKS
 import { useEffect, useState } from "react";
+// REACT ROUTER DOM
+import { NavLink } from "react-router-dom";
 // COMPONENT
 import Transition from "../../components/Transition";
 // EMAILJS
 import emailjs from "@emailjs/browser"
 // SWEETALERT
 import Swal from 'sweetalert2';
+// FRAMER MOTION
+import { motion } from "framer-motion";
+// REACT ICONS
+import { FaGithub, FaInstagram, FaLinkedinIn, FaWhatsapp } from "react-icons/fa6";
 
 const Contact = () => {
   const [name, setName] = useState("");
@@ -88,9 +94,102 @@ const Contact = () => {
   return (
     <Transition>
       <section className={styles.contact}>
-        <h2>
-          <span>//</span>  Contate <span>Me!</span>
-        </h2>
+        <div className={styles.header_container}>
+          <h2><span>//</span>  Contate <span>Me!</span></h2>
+
+          <div className={styles.social_media}>
+            <motion.div className={styles.icon_container}
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                duration: 3,
+                delay: 1.2,
+                ease: [0, 0.71, 0.2, 1.01],
+                scale: {
+                  type: "spring",
+                  damping: 5,
+                  stiffness: 100,
+                  restDelta: 0.001
+                }
+              }}
+            >
+              <NavLink to="https://api.whatsapp.com/send?phone=5585989295516"
+                className={styles.whatsapp_link}
+              >
+                <FaWhatsapp />
+              </NavLink>
+            </motion.div>
+
+            <motion.div className={styles.icon_container}
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                duration: 3,
+                delay: 1.5,
+                ease: [0, 0.71, 0.2, 1.01],
+                scale: {
+                  type: "spring",
+                  damping: 5,
+                  stiffness: 100,
+                  restDelta: 0.001
+                }
+              }}
+            >
+              <NavLink to="https://www.instagram.com/israelkilday/"
+                className={styles.instagram_link}
+              >
+                <FaInstagram />
+              </NavLink>
+            </motion.div>
+
+            <motion.div className={styles.icon_container}
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                duration: 0.3,
+                delay: 1.7,
+                ease: [0, 0.71, 0.2, 1.01],
+                scale: {
+                  type: "spring",
+                  damping: 5,
+                  stiffness: 100,
+                  restDelta: 0.001
+                }
+              }}
+            >
+              <NavLink to="https://www.linkedin.com/in/israeldevfrontend"
+                className={styles.linkedin_link}
+              >
+                <FaLinkedinIn />
+              </NavLink>
+            </motion.div>
+
+            <motion.div className={styles.icon_container}
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                duration: 3,
+                delay: 1.9,
+                ease: [0, 0.71, 0.2, 1.01],
+                scale: {
+                  type: "spring",
+                  damping: 5,
+                  stiffness: 100,
+                  restDelta: 0.001
+                }
+              }}
+            >
+              <NavLink to="https://github.com/Israelkilday"
+                className={styles.github_link}
+              >
+                <FaGithub />
+              </NavLink>
+            </motion.div>
+          </div>
+
+
+
+        </div>
 
         <form className={styles.form} onSubmit={sendEmail}>
           <div className={styles.input_box}>
