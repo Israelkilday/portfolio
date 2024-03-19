3f61273cb0bd52faff62b59cccc3f0afe965a2f9
Israel Kilday <israel.kilday@yahoo.com.br>
2024-03-19 00:03:49 -0300

    feat: modifying images of the projects

--- declaration.d.ts
+++ declaration.d.ts
@@ -8,3 +8,8 @@ declare module "*.png" {
     export default value;
   }
   
+declare module "*.jpg" {
+    const value: any;
+    export default value;
+  }
+  
--- src/pages/Portfolio/Portfolio.tsx
+++ src/pages/Portfolio/Portfolio.tsx
@@ -8,10 +8,10 @@ import { NavLink } from "react-router-dom";
 // COMPONENT
 import Transition from "../../components/Transition";
 // SWIPER
-import { Swiper, SwiperSlide } from "swiper/react"
+import { Swiper, SwiperSlide } from "swiper/react";
 import { EffectCoverflow } from "swiper/modules";
 // IMGAGENS
-import Portfolio_img0 from "../../../public/imgs/project_0.png"
+import Portfolio_img0 from "../../../public/imgs/project_0.jpg"
 import Portfolio_img1 from "../../../public/imgs/project_1.png"
 import Portfolio_img2 from "../../../public/imgs/project_2.png"
 
